baf14ff3bf0b9926ea07dc19478c66bcef07047a
Bart Calvin <bartcalvin@gmail.com>
2014-06-07 10:30:07 -0500

    first version

--- scripts/python/menu.py
+++ scripts/python/menu.py
@@ -1,32 +1,43 @@
 #!/usr/local/bin/python3.4
+
+## Imports
+import sys
 import subprocess
-## Ask the user to make a selection
-print("Please choose your task from the menu")
-## Print the menu
-print("""
+
+## menu function
+def menu():
+    ## Ask the user to make a selection
+    print("Please choose your task from the menu")
+    ## Print the menu
+    print("""
         1 - Show next available GID
         2 - Show next available UID
         3 - Exit
         """)
 
-## Ask for a selection
-selection = (input("Please make a selection from the menu: "))
+    ## Ask for a selection
+    selection = (input("Please make a selection from the menu: "))
 
-## Print the next available GID
-if selection == '1':
-    print('The next GID is as follows')
-    subprocess.call("/opt/admin-tools/scripts/bash/next-gid.sh", shell=True)
+    ## Print the next available GID
+    if selection == '1':
+        print('The next GID is as follows')
+        subprocess.call("/opt/admin-tools/scripts/bash/next-gid.sh", shell=True)
+        menu()
 
-## Print the next avaialbe UID
-elif selection == '2':
-    print('The next UID is as follows')
-    subprocess.call("/opt/admin-tools/scripts/bash/next-uid.sh", shell=True)
+    ## Print the next avaialbe UID
+    elif selection == '2':
+        print('The next UID is as follows')
+        subprocess.call("/opt/admin-tools/scripts/bash/next-uid.sh", shell=True)
+        menu()
 
-## Exit the script
-elif selection == '3':
-    print('Good bye')
+    ## Exit the script
+    elif selection == '3':
+        print('Good bye')
+        sys.exit()
 
-## This should kick you back into the menu to make another selection
-## not sure how to do this yet
-else:
-    print('Please try again')
+    ## Tell the user to try again and kick back into the function
+    else:
+        print('Please try again')
+        menu()
+
+menu()
